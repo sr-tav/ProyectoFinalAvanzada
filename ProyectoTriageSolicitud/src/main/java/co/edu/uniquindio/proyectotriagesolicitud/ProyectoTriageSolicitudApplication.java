9f998fcf61e1d4e3b35670ddf946a3c1edb72f2e
Tomas Aristizabal <159397819+sr-tav@users.noreply.github.com>
2026-03-16 10:19:19 -0500

    feat: Creacion del proyecto Spring boot

---
+++ ProyectoTriageSolicitud/src/main/java/co/edu/uniquindio/proyectotriagesolicitud/ProyectoTriageSolicitudApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectotriagesolicitud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTriageSolicitudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTriageSolicitudApplication.class, args);
+    }
+
+}
---
+++ ProyectoTriageSolicitud/src/test/java/co/edu/uniquindio/proyectotriagesolicitud/ProyectoTriageSolicitudApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectotriagesolicitud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTriageSolicitudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
