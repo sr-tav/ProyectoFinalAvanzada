3740689e12847ea0f81b46049e4bfbd55d6607d2
Tomas Aristizabal <159397819+sr-tav@users.noreply.github.com>
2026-03-16 17:49:43 -0500

    feat: creacion de entidades prt 1

---
+++ ProyectoTriageSolicitud/src/main/java/co/edu/uniquindio/proyectotriagesolicitud/dto/SolicitudRequest.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.proyectotriagesolicitud.dto;
+
+public class SolicitudRequest {
+}
--- ProyectoTriageSolicitud/src/main/java/co/edu/uniquindio/proyectotriagesolicitud/model/Solicitud.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.proyectotriagesolicitud.model;
-
-
-public class Solicitud {
-}
